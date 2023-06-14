b9613b0a22b0b8efdef44f60ecba3bcf1c838bee
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-06-14 21:40:23 +0200

    Sort geojson features by specified value

--- src/sources.js
+++ src/sources.js
@@ -13,6 +13,8 @@ module.exports = {
         fetch(def.url)
           .then(req => req.json())
           .then(json => {
+            json = modifySource(json, def)
+
             fs.writeFile(repConfig.path + '/' + key + '.geojson',
               JSON.stringify(json, null, '  '),
               (err) => done(err)
@@ -20,4 +22,28 @@ module.exports = {
           })
       }, (err) => callback(err))
   }
+}
+
+function modifySource (data, def) {
+  if (def.sortBy) {
+    data.features.sort((a, b) => {
+      const va = valueGet(a, def.sortBy.split('.'))
+      const vb = valueGet(b, def.sortBy.split('.'))
+      return va < vb ? -1 : 1
+    })
+  }
+
+  return data
+}
+
+function valueGet (data, path) {
+  const v = path[0] in data ? data[path[0]] : null
+  if (path.length > 1) {
+    if (v === 0) {
+      return null
+    }
+    return valueGet(v, path.slice(1))
+  }
+
+  return v
 }
