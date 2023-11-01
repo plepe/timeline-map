3396276bb339b24241710c6f1d5135895b93dc17
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-11-01 07:41:28 +0100

    Redesign controls, add a backward/forward button

--- src/timeline-controls.js
+++ src/timeline-controls.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 import App from './App'
 import state from './state'
 
@@ -12,6 +14,17 @@ App.addExtension({
       date = moment(inputs.date.value).format()
       app.updateLink()
       state.apply({ date })
+    })
+
+    ;['backward', 'forward'].forEach(v => {
+      inputs[v] = document.querySelector('button[name=' + v + ']')
+      inputs[v].addEventListener('click', () => {
+        let date = moment(state.get().date)
+        date = v == 'backward' ? date.subtract(1, 'M') : date.add(1, 'M')
+        date = date.format('YYYY-MM-DD')
+        state.apply({ date })
+        app.updateLink()
+      })
     })
 
     app.on('state-apply', state => {
