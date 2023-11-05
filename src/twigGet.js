fda828183bee99373de1a59d9281b04496c076a3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-11-05 07:45:37 +0100

    Expose openstreetmap-date-query as twig filter 'osmDateQuery'

--- src/twigGet.js
+++ src/twigGet.js
@@ -1,10 +1,12 @@
 const Twig = require('twig')
 import markers from 'openstreetbrowser-markers'
+const osmDateQuery = require('openstreetmap-date-query')
 
 Twig.extendFunction('markerLine', (data, options) => Twig.filters.raw(markers.line(data, options)))
 Twig.extendFunction('markerCircle', (data, options) => Twig.filters.raw(markers.circle(data, options)))
 Twig.extendFunction('markerPointer', (data, options) => Twig.filters.raw(markers.pointer(data, options)))
 Twig.extendFunction('markerPolygon', (data, options) => Twig.filters.raw(markers.polygon(data, options)))
+Twig.extendFilter('osmDateQuery', (value, options) => osmDateQuery(value, options[0]))
 
 const twigTemplates = {}
 
