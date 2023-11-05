e1299dec137c3cda24f45c0a99f57c40183fb276
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-11-05 07:46:44 +0100

    download_borders: include only outer/inner members of multipolygon

--- download_borders.js
+++ download_borders.js
@@ -3,7 +3,7 @@ const osmtogeojson = require('osmtogeojson')
 
 fetch('https://overpass-api.openhistoricalmap.org/api/map', {
   method: 'POST',
-  body: '[out:json][bbox:41.376,3.472,51.179,29.531];relation[admin_level=2][wikidata~"^Q(40|131964)$"];out body;>;out body;'
+  body: '[out:json][bbox:41.376,3.472,51.179,29.531];relation[admin_level=2][wikidata~"^Q(40|131964)$"];out body;(way(r:"outer");way(r:"inner"););out body geom;'
 })
 .then(req => req.json())
 .then(body => {
