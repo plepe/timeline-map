ed1b5b49c997489e38b80688f5588bb5a0f0ff97
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-11-18 08:19:16 +0100

    loader: implement caching

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,24 @@
+const cache = {}
+const loading = {}
+
 module.exports = function loader (url, options, callback) {
+  if (url in cache) {
+    return callback(null, cache[url])
+  }
+
+  if (url in loading) {
+    return loading[url].push(callback)
+  }
+
+  loading[url] = [callback]
+
   fetch(url)
     .then(req => req.json())
     .then(data => {
-      callback(null, data)
+      const cbs = loading[url]
+      delete loading[url]
+
+      cache[url] = data
+      cbs.forEach(cb => cb(null, data))
     })
 }
