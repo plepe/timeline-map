45633b9d5dac303ae99bc3ced74fd26ff276b468
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-11-25 14:55:21 +0000

    getTimespan: also work for single entry or null

--- src/getTimespan.js
+++ src/getTimespan.js
@@ -4,15 +4,23 @@ module.exports = function getTimespan (app) {
   return new Promise((resolve, reject) => {
     app.getParameter('timeline-timespan', 'all')
       .then(values => {
-        let start = values.map(v => v.start).filter(v => v).sort()
-        start = start.length ? start[0] : app.config.timeline.defaultMin
-        let end = values.map(v => v.end).filter(v => v).sort().reverse()
-        end = end.length ? end[0] : app.config.timeline.defaultMax
+        if (!values) {
+          return resolve({ start: null, end: null })
+        }
 
-        start = completeDate(start, 'start')
-        end = completeDate(end, 'end')
+        if (Array.isArray(values)) {
+          let start = values.map(v => v.start).filter(v => v).sort()
+          start = start.length ? start[0] : app.config.timeline.defaultMin
+          let end = values.map(v => v.end).filter(v => v).sort().reverse()
+          end = end.length ? end[0] : app.config.timeline.defaultMax
 
-        resolve({ start, end })
+          start = completeDate(start, 'start')
+          end = completeDate(end, 'end')
+
+          return resolve({ start, end })
+        }
+
+        resolve(values)
       })
   })
 }
