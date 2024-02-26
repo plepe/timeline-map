04de24e2b6cce96843f78f6968cd8f61ab580fe0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-02-26 20:23:52 +0100

    twigGet: use the geowiki-viewer's 'twigGet' function

--- src/twigGet.js
+++ src/twigGet.js
@@ -12,25 +12,4 @@ Twig.extendFilter('json_decode', (value) => {
 
 const twigTemplates = {}
 
-module.exports = function twigGet (template, data, callback) {
-  if (typeof template !== 'string') {
-    console.error('Twig template is not a string:', template)
-    return template
-  }
-
-  if (!(template in twigTemplates)) {
-    try {
-      twigTemplates[template] = Twig.twig({ data: template, rethrow: true })
-    } catch (e) {
-      console.error('Error compiling Twig template:', template, e.message)
-      return ''
-    }
-  }
-
-  if (callback) {
-    twigTemplates[template].renderAsync(data)
-      .then(result => callback(null, result.trim()))
-  } else {
-    return twigTemplates[template].render(data).trim()
-  }
-}
+module.exports = require('geowiki-viewer/src/twigGet')
